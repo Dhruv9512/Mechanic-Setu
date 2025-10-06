be2d98fd3d6cc8405fb0b0b0f82e8edc76f058eb
man-navlakha <dhruvsharma56780@gmail.com>
2025-10-06 18:16:38 +0530

    fix web socket connection

--- MechanicSetu/asgi.py
+++ MechanicSetu/asgi.py
@@ -4,12 +4,19 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import jobs.routing
+import django
 
+# Set the settings module environment variable
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MechanicSetu.settings')
 
+
+django.setup()
+
+# Import your routing module AFTER Django is initialized
+import jobs.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": get_asgi_application(),  # Use the initialized app for HTTP
     "websocket": AuthMiddlewareStack(
         URLRouter(
             jobs.routing.websocket_urlpatterns
