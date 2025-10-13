400239de2302766fd3259901042d24d4e2941df5
man-navlakha <dhruvsharma56780@gmail.com>
2025-10-13 21:07:59 +0530

    fix

--- Profile/urls.py
+++ Profile/urls.py
@@ -1,7 +1,10 @@
-
-from django.contrib import admin
 from django.urls import path
+from .views import UserProfileView,UserServiceRequestHistoryView,EditUserProfileView
+
+
 
 urlpatterns = [
-    
+    path('UserProfile/', UserProfileView.as_view(), name='User Profile'),
+    path('EditUserProfile/', EditUserProfileView.as_view(), name='EditUser Profile'),
+    path('UserHistory/', UserServiceRequestHistoryView.as_view(), name='User History'),
 ]
