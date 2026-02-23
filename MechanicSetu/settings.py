578de4205689d201a63b4396ddaa600cb4bfa421
Dhruv Sharma <dhruvsharma56780@gmail.com>
2026-02-23 22:03:54 +0530

    'USER_ID_FIELD': 'id',     'USER_ID_CLAIM': 'user_id'

--- MechanicSetu/settings.py
+++ MechanicSetu/settings.py
@@ -117,7 +117,9 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),      
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),        
     'ROTATE_REFRESH_TOKENS': True,                       
-    'BLACKLIST_AFTER_ROTATION': True,                    
+    'BLACKLIST_AFTER_ROTATION': True,
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id'                    
 }
 
 ROOT_URLCONF = "MechanicSetu.urls"
